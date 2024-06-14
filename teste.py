2bae0319d9a2dc41be55ad8ed59040ce06eaa1b7
BrunoSantos751 <Brunomoraesleao@hotmail.com>
2024-06-13 21:17:22 -0300

    parte do bruno feita

--- teste.py
+++ teste.py
@@ -1,1 +1,24 @@
-print('uepaa')
+import sqlite3 as sql
+
+conexao = sql.connect('dados.bd')
+cursor= conexao.cursor()
+
+comando = 'CREATE TABLE if not exists livros (ISBN INTEGER PRIMARY KEY, TITULO VARCHAR(255), AUTOR VARCHAR(255), anoPublicação INTEGER, genero VARCHAR (255) )'
+cursor.execute(comando)
+
+comando = 'CREATE TABLE if not exists emprestimos (ISBN INTEGER PRIMARY KEY, TITULO VARCHAR(255), Membro VARCHAR(255), dataEmprestimo DATE, dataDevolucao date )'
+cursor.execute(comando)
+
+def select():
+    comando = 'SELECT * from emprestimos'
+    cursor.execute(comando)
+    a=cursor.fetchall()
+    print (a)
+
+def insert():
+    inserir = 'insert into emprestimos values(?, ?, ?, ?, ?)'
+    regitros = (4, "teste4 ", "andre", 2024, "acao" )
+    cursor.execute(inserir, regitros)
+    conexao.commit()
+
+select()
